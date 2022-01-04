b05e86b0e0fc1e351e42871d90a20433f6da476b
Oleg <vetti.ch@ya.ru>
2022-01-04 05:01:03 +0500

    feat: add update-version.sh

---
+++ bin/update-version.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+VERSION=$1
+
+sed -i -E "/\* Version:/s/[0-9].[0-9].[0-9]/$VERSION/g" parrotposter.php
+sed -i -E "/PARROTPOSTER_VERSION/s/[0-9].[0-9].[0-9]/$VERSION/g" parrotposter.php
+sed -i -E "/Stable tag:/s/[0-9].[0-9].[0-9]/$VERSION/g" readme.txt
+
